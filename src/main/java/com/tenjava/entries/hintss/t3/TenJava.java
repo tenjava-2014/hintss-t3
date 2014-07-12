70fb7176299135ae1ac289cc492a8cbd8b237202
Henry Ammermann <hintsss@gmail.com>
2014-07-12 07:14:18 -0700

    I made an onEnable()!

--- src/main/java/com/tenjava/entries/hintss/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/hintss/t3/TenJava.java
@@ -2,4 +2,9 @@ package com.tenjava.entries.hintss.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
